2c3652626ba054d7bf17c1fc1f2ab20031457363
Toshi <dtkellogg10@gmail.com>
2023-02-02 11:13:41 -0400

    Screens - Place Order Screen built

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -98,7 +98,7 @@ const BasketScreen = () => {
             <Text className="font-extrabold">${basketTotal + 5.99}</Text>
           </View>
 
-          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+          <TouchableOpacity onPress={() => navigation.navigate("PreparingOrder")} className="rounded-lg bg-[#00CCBB] p-4">
             <Text className="text-center text-white text-lg font-bold">Place Order</Text>
           </TouchableOpacity>
         </View>
