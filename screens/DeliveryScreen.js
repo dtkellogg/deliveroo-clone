092cc780cc548a7922a12742f5f2df233d6c28d4
Toshi <dtkellogg10@gmail.com>
2023-02-02 11:47:51 -0400

    Screens - Delivery Screen built

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -6,6 +6,7 @@ import { selectRestaurant } from '../features/restaurantSlice'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { XMarkIcon } from 'react-native-heroicons/solid'
 import * as Progress from 'react-native-progress'
+import MapView, { Marker } from "react-native-maps"
 
 const DeliveryScreen = () => {
   const navigation = useNavigation()
@@ -41,6 +42,49 @@ const DeliveryScreen = () => {
             Your order at {restaurant.title} is being prepared
           </Text>
         </View>
+      </SafeAreaView>
+
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitute: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="flex-1 -mt-10 z-0"
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitute: restaurant.long,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="#00CCBB"
+        />
+      </MapView>
+
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru"
+          }}
+          className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">
+            Toshi Kellogg
+          </Text>
+          <Text className="text-gray-400">
+            Your Rider
+          </Text>
+        </View>
+
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold">
+          Call
+        </Text>
       </SafeAreaView>
     </View>
   )
